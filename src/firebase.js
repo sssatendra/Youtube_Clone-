0fb7bf582f7c858694773c5efb2e9a70a398ede7
sssatendra <74877594+sssatendra@users.noreply.github.com>
2021-04-20 16:55:55 +0530

    Initialize project using Create React App

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBr8apoZETpdgO7NG1iSON-9hRmfnJi0xk",
+  authDomain: "you-tube-720ed.firebaseapp.com",
+  projectId: "you-tube-720ed",
+  storageBucket: "you-tube-720ed.appspot.com",
+  messagingSenderId: "656631242653",
+  appId: "1:656631242653:web:5fe092e95569a821869974"
+};
+
+export default firebaseConfig;
